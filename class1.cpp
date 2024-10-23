2e8658e2533d40bad48f263b672885fe4d77ca35
johra_01 <148644216+johranayab@users.noreply.github.com>
2024-10-23 22:57:40 +0530

    Add files via upload

---
+++ class1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+ int main (){
+    cout<<"hellow world/n from me";
+cout<<"hellow world/n from johra nayab";
+
+cout<<"hellow world/n from johra nayab";
+cout<<"hellow world/n from johra nayab";
+    return 0;
+ }
